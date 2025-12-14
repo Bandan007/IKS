a749b1ec1ce940dc3b328b3316ee706efc93e40a
Bandan007 <bandandeykarmakar@proton.me>
2025-12-14 14:43:11 +0530

    Update main.js

--- main.js
+++ main.js
@@ -16,23 +16,3 @@ function scaleSlides() {
 window.addEventListener("resize", scaleSlides);
 window.addEventListener("load", scaleSlides);
 
-//[let slides = document.querySelectorAll(".slide");
-let index = 0;
-
-function showSlide(i) {
-  slides.forEach(s => s.style.display = "none");
-  slides[i].style.display = "block";
-}
-
-showSlide(index);
-
-document.addEventListener("keydown", (e) => {
-  if (e.key === "ArrowRight") {
-    index = Math.min(index + 1, slides.length - 1);
-    showSlide(index);
-  }
-  if (e.key === "ArrowLeft") {
-    index = Math.max(index - 1, 0);
-    showSlide(index);
-  }
-});]
